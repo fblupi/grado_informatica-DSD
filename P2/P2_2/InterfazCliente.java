68d9ba46608c3504fe839c03fc288714600643e8
Francisco Javier Bolívar Lupiáñez <fboli94@gmail.com>
2015-05-22 17:04:04 +0200

    GUI del segundo ejercicio empezada

---
+++ P2/P2_2/InterfazCliente.java
@@ -0,0 +1,6 @@
+import java.rmi.Remote;
+import java.rmi.RemoteException;
+
+public interface InterfazCliente extends Remote {
+	public void mostrarMensaje (String cliente, String mensaje) throws RemoteException;
+}
